1853878078d378c7b6e1a3f785c2fc06c5f21204
idhun90 <idhun90@naver.com>
2022-08-06 18:39:31 +0900

    컬렉션뷰 Cell 레이아웃 구현 오토레이아웃 적용

---
+++ TMDBProject2/MainCollectionViewCell.swift
@@ -0,0 +1,64 @@
+//
+//  MainCollectionViewCell.swift
+//  TMDBProject2
+//
+//  Created by 이도헌 on 2022/08/06.
+//
+
+import UIKit
+
+class MainCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var inView: UIView!
+    @IBOutlet weak var voteview: UIView!
+    @IBOutlet weak var voteTextLabel: UILabel!
+    @IBOutlet weak var voteIntView: UIView!
+    @IBOutlet weak var voteIntLabel: UILabel!
+    @IBOutlet weak var linkButton: UIButton!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overViewLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layout()
+    }
+    
+    func layout() {
+        
+        inView.layer.masksToBounds = true
+        inView.layer.cornerRadius = 7
+        
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowRadius = 7
+        self.layer.shadowOpacity = 0.2
+        self.layer.shadowOffset = CGSize(width: 3, height: 3)
+        
+        dateLabel.font = .systemFont(ofSize: 12)
+        genreLabel.font = .boldSystemFont(ofSize: 17)
+        genreLabel.text = "장르"
+        
+        titleLabel.font = .systemFont(ofSize: 20, weight: .bold)
+        overViewLabel.font = .systemFont(ofSize: 17)
+        overViewLabel.textColor = .darkGray
+        
+        detailLabel.font = .systemFont(ofSize: 15)
+        detailLabel.text = "자세히 보기"
+        
+        voteview.backgroundColor = .systemPurple
+        voteTextLabel.font = .systemFont(ofSize: 14)
+        voteTextLabel.textColor = .white
+        voteIntView.backgroundColor = .white
+        voteIntLabel.font = .systemFont(ofSize: 14)
+        
+        linkButton.tintColor = .white
+        
+    }
+
+}
--- TMDBProject2/MainViewController.swift
+++ TMDBProject2/MainViewController.swift
@@ -2,9 +2,45 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var MainCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        MainCollectionView.delegate = self
+        MainCollectionView.dataSource = self
+        
+        let nib = UINib(nibName: MainCollectionViewCell.ReusableIdentifier, bundle: nil)
+        MainCollectionView.register(nib, forCellWithReuseIdentifier: MainCollectionViewCell.ReusableIdentifier)
+        collectionViewLayout()
+    }
+    
+    func collectionViewLayout() {
+        let layout = UICollectionViewFlowLayout()
+        let spacing: CGFloat = 7
+        let itemCount: CGFloat = 1
+        
+        let width = (UIScreen.main.bounds.width - spacing * (itemCount + 1)) / itemCount
+        layout.itemSize = CGSize(width: width, height: width)
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        
+        MainCollectionView.collectionViewLayout = layout
     }
 
+}
+
+//MARK: - CollectionView Protocol 채택
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = MainCollectionView.dequeueReusableCell(withReuseIdentifier: MainCollectionViewCell.ReusableIdentifier, for: indexPath) as? MainCollectionViewCell else { return UICollectionViewCell() }
+        
+        return cell
+    }
 }
