80c7dea422185188850e0ecfeec71ccdcc317099
idhun90 <idhun90@naver.com>
2022-08-12 02:45:27 +0900

    주변 영화관 맵뷰 진행 중

---
+++ Map/TheaterViewController.swift
@@ -0,0 +1,143 @@
+import UIKit
+import MapKit
+import CoreLocation
+
+class TheaterViewController: UIViewController {
+    
+    @IBOutlet weak var TheaterMapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    let theaterList = TheaterList()
+    let centerList: [CLLocationCoordinate2D] = []
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager.delegate = self
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(showTheaterActionSheet))
+        
+        setRegionAndAnnotation()
+        
+        print(theaterList.mapAnnotations[0].location)
+        print(theaterList.mapAnnotations.count)
+        
+    }
+    
+    func setRegionAndAnnotation() {
+        
+        for count in 0...theaterList.mapAnnotations.count-1 {
+            let center = CLLocationCoordinate2D(latitude: theaterList.mapAnnotations[count].latitude, longitude: theaterList.mapAnnotations[count].longitude)
+//            let region = MKCoordinateRegion(center: center, latitudinalMeters: 5000, longitudinalMeters: 5000)
+//            TheaterMapView.setRegion(region, animated: true)
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = center
+            annotation.title = theaterList.mapAnnotations[count].location
+            TheaterMapView.addAnnotation(annotation)
+        }
+        
+        //  TheaterMapView.addAnnotations(<#T##annotations: [MKAnnotation]##[MKAnnotation]#>)
+    }
+    //
+    @objc func showTheaterActionSheet() {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let megabox = UIAlertAction(title: "메가박스", style: .default) { _ in
+            
+        }
+        
+        let lotte = UIAlertAction(title: "롯데시네마", style: .default) { _ in
+            
+        }
+        
+        let cgv = UIAlertAction(title: "CGV", style: .default) { _ in
+            
+        }
+        
+        let all = UIAlertAction(title: "전체보기", style: .default) { _ in
+            
+        }
+        
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        alert.addAction(megabox)
+        alert.addAction(lotte)
+        alert.addAction(cgv)
+        alert.addAction(all)
+        alert.addAction(cancel)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+}
+
+extension TheaterViewController {
+    
+    // iOS 버전에 따른 분기 처리 및 iOS 위치서비스 활성화 여부 체크
+    func checkDeviceLocationServiceAuthorzation() {
+        print(#function)
+        
+        let authorizationStatus: CLAuthorizationStatus
+        
+        // iOS 버전에 따른 사용할 코드 분기 처리
+        if #available(iOS 14.0, *) {
+            authorizationStatus = locationManager.authorizationStatus
+        } else {
+            authorizationStatus = CLLocationManager.authorizationStatus()
+        }
+        
+        // 디바이스 위치서비스 활성화 여부 체크
+        if CLLocationManager.locationServicesEnabled() {
+            checkUserCurrentLocationAuthorization(authorizationStatus)
+        } else {
+            print("위치 서비스가 비활성화되어 있습니다.")
+        }
+    }
+    
+    // 사용자가 선택한 앱의 위치 접근 허용 상태 체크
+    func checkUserCurrentLocationAuthorization(_ authorizationStatus: CLAuthorizationStatus) {
+        switch authorizationStatus {
+        case .notDetermined:
+            print("대기 상태, notDetermined")
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted, .denied:
+            print("")
+        case .authorizedAlways:
+            print("항상 허용 권한, 먼저 WhenInUse 권한 요청 후 처리하기")
+        case .authorizedWhenInUse:
+            print("앱을 사용하는 동안 허용")
+            locationManager.startUpdatingLocation()
+        default:
+            print("오류 발생")
+        }
+    }
+}
+
+extension TheaterViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(#function)
+        
+        if let coordinate = locations.last?.coordinate {
+            
+        }
+        
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(#function)
+        print("사용자의 위치를 가져오지 못 했습니다.")
+    }
+    
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        print(#function)
+        checkDeviceLocationServiceAuthorzation()
+    }
+}
